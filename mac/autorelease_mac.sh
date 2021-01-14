e97af7d4fc511c305af5f1bbeaa7dcf32b18afc5
ann0see <20726856+ann0see@users.noreply.github.com>
2021-01-14 13:52:54 +0100

    Syntax fix

--- mac/autorelease_mac.sh
+++ mac/autorelease_mac.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # please run this script with the first parameter being the root of the repo
-if [ -z "${1}"]; then
+if [ -z "${1}" ]; then
     echo "Please give the path to the repository root as second parameter to this script!"
     exit 1
 fi
