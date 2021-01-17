f9b1c8d25a4c9c91636dcbbde3e8baf07541c050
ann0see <20726856+ann0see@users.noreply.github.com>
2021-01-17 20:46:10 +0100

    Enable autobuild on Windows

--- windows/autorelease_windows.bat
+++ windows/autorelease_windows.bat
@@ -1,1 +1,12 @@
 # Sets up the environment for autobuild on Windows
+
+# install Qt
+powershell pip install aqtinstall
+aqt install --outputdir C:\Qt 5.15.2 windows desktop win64_msvc2019_64
+aqt install --outputdir C:\Qt 5.15.2 windows desktop win32_msvc2019
+
+# build the installer
+powershell %1\windows\deploy_windows.ps1 C:\Qt\5.15.2
+
+# rename the file
+cp %1\deploy\Jamulus*installer-win.exe %1\deploy\Jamulus-installer-win.exe
