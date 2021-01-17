a8504d832407041d156c74109721606d50942dc3
ann0see <20726856+ann0see@users.noreply.github.com>
2021-01-17 20:50:23 +0100

    Fix comments

--- windows/autorelease_windows.bat
+++ windows/autorelease_windows.bat
@@ -1,12 +1,12 @@
-# Sets up the environment for autobuild on Windows
+:: Sets up the environment for autobuild on Windows
 
-# install Qt
+:: Install Qt
 powershell pip install aqtinstall
 aqt install --outputdir C:\Qt 5.15.2 windows desktop win64_msvc2019_64
 aqt install --outputdir C:\Qt 5.15.2 windows desktop win32_msvc2019
 
-# build the installer
+:: Build the installer
 powershell %1\windows\deploy_windows.ps1 C:\Qt\5.15.2
 
-# rename the file
+:: Rename the installer
 cp %1\deploy\Jamulus*installer-win.exe %1\deploy\Jamulus-installer-win.exe
