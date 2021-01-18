a223f2f06de2d4137cb8f45b5123f90639c81381
ann0see <20726856+ann0see@users.noreply.github.com>
2021-01-18 15:13:05 +0100

    Bug fix: Rename variable

--- .github/actions_scripts/get_release_vars.sh
+++ .github/actions_scripts/get_release_vars.sh
@@ -11,4 +11,4 @@ PUSHED_NAME=${GITHUB_REF#refs/*/}
 # calculate various variables
 python3 ${1}/.github/actions_scripts/analyse_git_reference.py ${GITHUB_REF} ${PUSHED_NAME} ${JAMULUS_VERSION} 
 
-perl ${1}/.github/actions_scripts/getChangelog.pl ${1}/ChangeLog ${VERSION} > ${1}/autoLatestChangelog.md
+perl ${1}/.github/actions_scripts/getChangelog.pl ${1}/ChangeLog ${JAMULUS_VERSION} > ${1}/autoLatestChangelog.md
