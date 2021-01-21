a3fe118a7e983c058640453ae1c1c454a75369e3
ann0see <20726856+ann0see@users.noreply.github.com>
2021-01-21 07:51:31 +0100

    Add quotes

--- .github/actions_scripts/get_release_vars.sh
+++ .github/actions_scripts/get_release_vars.sh
@@ -3,12 +3,12 @@
 set -e 
 
 # get version from project-file
-JAMULUS_VERSION=$(cat ${1}/Jamulus.pro | grep -oP 'VERSION = \K\w[^\s\\]*')
+JAMULUS_VERSION="$(cat ${1}/Jamulus.pro | grep -oP 'VERSION = \K\w[^\s\\]*')"
 
 # get the tag/branch-name which pushed this
 PUSHED_NAME=${GITHUB_REF#refs/*/}
 
 # calculate various variables
-python3 ${1}/.github/actions_scripts/analyse_git_reference.py ${GITHUB_REF} ${PUSHED_NAME} ${JAMULUS_VERSION} 
+python3 "${1}"/.github/actions_scripts/analyse_git_reference.py "${GITHUB_REF}" "${PUSHED_NAME}" "${JAMULUS_VERSION}"
 
-perl ${1}/.github/actions_scripts/getChangelog.pl ${1}/ChangeLog ${JAMULUS_VERSION} > ${1}/autoLatestChangelog.md
+perl "${1}"/.github/actions_scripts/getChangelog.pl "${1}"/ChangeLog "${JAMULUS_VERSION}" > "${1}"/autoLatestChangelog.md
